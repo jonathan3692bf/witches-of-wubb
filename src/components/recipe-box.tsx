a6917f25149b9c548c51dc86674c303e23aedca6
Jonathan <jonathan3692bf@users.noreply.github.com>
2023-06-06 16:12:01 -0700

    Adjust viewport, update Music Database, and generate spells even when empty

--- src/components/recipe-box.tsx
+++ src/components/recipe-box.tsx
@@ -10,20 +10,18 @@ export default function RecipeBox() {
   return (
     <div id='inner_recipe_box' className='max-w-full mt-[140px]'>
       <div id='recipe-bg' className='h-[23vh] w-full max-h-full mx-auto bg-recipe-bg'>
-        <div id='recipe-header' className='h-[80px] grid grid-cols-3'>
-          <div
-            id='title'
-            className='flex items-center stroke-black justify-center gap-6 col-start-2 text-xl -top-[20px] align-top font-fondamento'
-          >
-            Suggested Recipe: {spellName}
+        <div id='recipe-header' className='flex w-screen relative align-center justify-center'>
+          <div id='title' className='font-fondamento stroke-black flex gap-2 my-5  text-2xl'>
+            <div>Suggested Recipe:</div>
+            <div>{spellName}</div>
           </div>
-          <div id='new-spell' className='float-right'>
+          <div id='new-spell' className='absolute right-0'>
             <button
               id='new-spell-btn'
               style={{ backgroundImage: `url(/images/new-spell.gif)` }}
-              className='bg-cover h-[100px] w-[100px] stroke-black font-fondamento text-white py-2 px-4 rounded transition duration-200 ease-in-out hover:bg-opacity-50 float-right mr-8 mt-3'
+              className='bg-cover h-[100px] w-[100px] py-2 px-4 rounded transition duration-200 ease-in-out hover:bg-opacity-50'
               onClick={generateNewSpell}
-            ></button>
+            />
           </div>
         </div>
         <div id='ingredients_contianer' className='grid grid-cols-4 text-center align-top'>
--- src/hooks/use-grimoire.ts
+++ src/hooks/use-grimoire.ts
@@ -5,6 +5,52 @@ import { RFIDToClipMap } from '~/lib/database-output';
 
 export type SpellRecipeType = { [key: string]: ClipMetadataType };
 
+const SPELL_NAMES = [
+  'Dancing Unicorn Vibez',
+  'The Frog Princex',
+  'Happily Never After',
+  'The Rite of Spring Fling',
+  'Bling Shot',
+  'Cup in Smoke',
+  'Unconvictional Love',
+  'Fleeting Sex Appeal',
+  'Cosmic Apotheosis',
+  'Victory Sound',
+  'Cheers for Fears',
+  'Inner Demon Lure',
+  'Self a Steam',
+  'Mind Fog',
+  'Woo Goo',
+  'Immorality',
+  'Cockroaching',
+  'Seduction Production',
+  'Even Higher Now',
+  'Curse Reverse',
+  'Bad Witch Boogie',
+  'Aurora Aura',
+  'Essence of Hell',
+  'Twerking Toadstool',
+  'Wand from the Pond',
+  'Telepathic Erection',
+  'Pole Gazing',
+  'Rebirthing Rumba',
+  'Black Hole Boom Boom',
+  'Astral Ejecting',
+  'Celestial Soup',
+  'Starlight Shuffle',
+  'Swirling Dervish',
+  "Big Simpin'",
+  'Songs of the Subs',
+  'Badonka-Dom',
+  'Pagan Popper',
+  'Bone Drone',
+  'Flower Shower',
+];
+
+function ChooseRandomElementFrom(array: any[]) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 export default function useGrimoire() {
   const { playingClips } = useContext(AbletonContext);
   const [spellRecipe, setSpellRecipe] = useState<SpellRecipeType>({});
@@ -19,54 +65,22 @@ export default function useGrimoire() {
     });
 
   function generateNewSpellName() {
-    const listOfSpellsNames = [
-      'Dancing Unicorn Vibez',
-      'The Frog Princex',
-      'Happily Never After',
-      'The Rite of Spring Fling',
-      'Bling Shot',
-      'Cup in Smoke',
-      'Unconvictional Love',
-      'Fleeting Sex Appeal',
-      'Cosmic Apotheosis',
-      'Victory Sound',
-      'Cheers for Fears',
-      'Inner Demon Lure',
-      'Self a Steam',
-      'Mind Fog',
-      'Woo Goo',
-      'Immorality',
-      'Cockroaching',
-      'Seduction Production',
-      'Even Higher Now',
-      'Curse Reverse',
-      'Bad Witch Boogie',
-      'Aurora Aura',
-      'Essence of Hell',
-      'Twerking Toadstool',
-      'Wand from the Pond',
-      'Telepathic Erection',
-      'Pole Gazing',
-      'Rebirthing Rumba',
-      'Black Hole Boom Boom',
-      'Astral Ejecting',
-      'Celestial Soup',
-      'Starlight Shuffle',
-      'Swirling Dervish',
-      "Big Simpin'",
-      'Songs of the Subs',
-      'Badonka-Dom',
-      'Pagan Popper',
-      'Bone Drone',
-      'Flower Shower',
-    ];
-
-    setSpellName(listOfSpellsNames[Math.floor(Math.random() * listOfSpellsNames.length)]);
+    setSpellName((name) => {
+      let newName = ChooseRandomElementFrom(SPELL_NAMES);
+      while (newName === name) {
+        newName = ChooseRandomElementFrom(SPELL_NAMES);
+      }
+      return newName;
+    });
   }
 
   function generateNewSpell() {
-    const randomClip =
-      actuallyPlayingClips[Math.floor(Math.random() * actuallyPlayingClips.length)];
+    let randomClip;
+    if (actuallyPlayingClips.length) {
+      randomClip = ChooseRandomElementFrom(actuallyPlayingClips);
+    } else {
+      randomClip = ChooseRandomElementFrom(Object.values(RFIDToClipMap));
+    }
 
     const newSpell = {} as SpellRecipeType;
     for (const type of Object.keys(ClipTypes)) {
@@ -81,10 +95,7 @@ export default function useGrimoire() {
   }
 
   useEffect(() => {
-    if (actuallyPlayingClips.length === 0) {
-      setSpellRecipe({});
-      setSpellName('');
-    } else if (actuallyPlayingClips.length === 1) {
+    if (actuallyPlayingClips.length <= 1) {
       generateNewSpell();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
