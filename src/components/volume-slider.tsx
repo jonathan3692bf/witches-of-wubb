6deec43c86df062bddc0a6d92ec9f2ad7756772a
Jonathan <jonathan3692bf@users.noreply.github.com>
2023-06-06 17:21:10 -0700

    Change VolumeSlider to use a max value of 0.7 and a reset value of 0.6

--- src/components/volume-slider.tsx
+++ src/components/volume-slider.tsx
@@ -3,10 +3,12 @@ import { AbletonContext } from '../contexts/ableton-provider';
 // import { LoggerContext } from '../contexts/logger-provider';
 
 const MIN_VALUE = 0;
-const MAX_VALUE = 1;
+const MAX_VALUE = 0.7;
+const RESET_VALUE = 0.6;
 export default function VolumeSlider({ pillar }: { pillar: number }) {
-  const { trackVolume, changeTrackVolume } = useContext(AbletonContext);
   // const { logger } = useContext(LoggerContext);
+  const { trackVolume, changeTrackVolume } = useContext(AbletonContext);
+  const value = trackVolume[pillar] ? Math.max(trackVolume[pillar], 0.7) : 0;
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const newVolume = Number(e.target.value);
@@ -14,33 +16,29 @@ export default function VolumeSlider({ pillar }: { pillar: number }) {
   }
 
   function resetVolume() {
-    changeTrackVolume({ pillar, volume: 0.85 });
+    changeTrackVolume({ pillar, volume: RESET_VALUE });
   }
 
   return (
     <>
-      <div id={`${pillar}-volume-range`} className='w-full max-h-full text-center flex flex-col'>
+      <div id={`${pillar}-volume-range`} className='w-full max-h-full flex flex-col items-center'>
         <label
           htmlFor={`${pillar}-volume-range`}
-          className='block mb-2 text-sm font-medium stroke-black font-fondamento'
+          className={
+            'block mb-2 text-sm font-medium stroke-black font-fondamento' +
+            (pillar % 2 === 0 ? ' text-left' : '')
+          }
         >
           Volume
         </label>
         <div className='flex flex-row text-lg max-h-full gap-4 justify-center'>
-          <div className='flex flex-col justify-between'>
-            {/* <div className=' text-gray-500 dark:text-gray-400'>{MAX_VALUE}</div>
-            <div className=' text-gray-500 dark:text-gray-400'>{MIN_VALUE}</div> */}
-          </div>
-          {/* <div className='flex flex-col justify-center text-xl'>
-            {Math.round((trackVolume[pillar] ?? 0) * 100)}
-          </div> */}
           <input
             id={`${pillar}-volume-range`}
             type='range'
             min={MIN_VALUE}
             max={MAX_VALUE}
             step={0.01}
-            value={trackVolume[pillar] ?? 0}
+            value={value}
             onChange={handleChange}
             orient='vertical'
             className='w-[20px] h-[170px] rounded-lg appearance-none cursor-pointer bg-gray-200 dark:bg-gray-700 accent-red-800 custom-volume-slider'
@@ -48,7 +46,7 @@ export default function VolumeSlider({ pillar }: { pillar: number }) {
         </div>
         <button
           onClick={resetVolume}
-          className='bg-white font-fondamento hover:bg-gray-100 text-gray-800 font-semibold px-1 w-min border border-gray-400 rounded shadow mx-auto mt-4'
+          className='bg-white font-fondamento hover:bg-gray-100 text-gray-800 font-semibold px-1 w-min border border-gray-400 rounded shadow mt-4'
         >
           Reset
         </button>
